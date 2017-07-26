77259a5a106c5a22b55de089429d16dc485171d9
ASIAPACIFIC\chenluo <luo-jie.chen@hpe.com>
2017-07-26 10:15:54 +0800

    M1 RELEASE

--- resource-server-jwt-feign/src/main/java/com/springcloud/template/resourceserverjwtfeign/controller/FeignTestController.java
+++ resource-server-jwt-feign/src/main/java/com/springcloud/template/resourceserverjwtfeign/controller/FeignTestController.java
@@ -3,6 +3,7 @@ package com.springcloud.template.resourceserverjwtfeign.controller;
 import com.springcloud.template.resourceserverjwtfeign.feign.ResourceServerJwtClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,12 @@ public class FeignTestController {
         log.info("feign hello");
         return resourceServerJwtClient.helloJwt();
 
+    }
+    @RequestMapping("current")
+    public String currentUser(){
+        String name= SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
+        log.info("current user {}",name);
+        return "current user :"+name;
     }
 
 }
